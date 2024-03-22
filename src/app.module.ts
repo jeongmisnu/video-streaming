00bca55e04a5ea9067672714b5a232b6ea67a90f
shan <ansuz0616@naver.com>
2024-03-22 17:06:55 +0900

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfig } from '@app/databases/postgres/postgres.config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfig } from '@app/databases/mongodb/mongoose.config';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-ioredis';
+import { UserModule } from './routers/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'prod' ? '.prod.env' : '.dev.env',
+      isGlobal: true,
+    }),
+    CacheModule.register({
+      store: redisStore,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: PostgresConfig,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: MongooseConfig,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
